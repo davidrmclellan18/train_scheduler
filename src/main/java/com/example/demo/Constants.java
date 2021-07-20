26b32e66baf62f333df327647ff42c95b512c749
davidrmclellan18 <83520158+davidrmclellan18@users.noreply.github.com>
2021-07-20 14:26:00 -0700

    Create Constants.java
    constants that never change

---
+++ src/main/java/com/example/demo/Constants.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class Constants {
+    private static final String BASE_URL = "https://api-v3.mbta.com/schedules";
+    private static final String HOUR_TIME_FORMAT = "HH:mm";
+    private static final String YEAR_DATE_FORMAT = "yyyy-MM-dd";
+    private static final String TIME_ZONE = "America/New_York";
+    private static final String ARRIVAL_TIME = "arrival_time";
+    private static final String DEPARTURE_TIME = "departure_time";
+    private static final String ARRIVALS = "arrivals";
+    private static final String DEPARTURES = "departures";
+
+}
